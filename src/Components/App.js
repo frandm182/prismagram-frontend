81031b60e18b52e414adbc3ccfc6edeb27952412
FRANCISCO JAVIER DIAZ MARTIN <franciscojavier.diaz.martin.contractor@bbva.com>
2021-01-14 15:45:51 +0100

    add routes and refact node modules

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from 'react-apollo-hooks';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
+import client from '../Apollo/Client';
+import AppRouter from './Router';
 
 const App = () => (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-    Hola
+    <ApolloProvider client={client}>
+      <>
+      <GlobalStyles />
+      <AppRouter isLoggedIn={false}/>
+      </>
+    </ApolloProvider>
   </ThemeProvider>);
 
 export default App;
