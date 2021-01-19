0bc6dc89d72de8f5c347ec63a0249bebbb4f5544
FRANCISCO JAVIER DIAZ MARTIN <franciscojavier.diaz.martin.contractor@bbva.com>
2021-01-19 09:33:30 +0100

    add feed query

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,2 +1,41 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default () => 'Feed';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from 'react-apollo-hooks';
+
+const FEED_QUERY = gql`
+{
+  seeFeed {
+    id
+    location
+    caption
+    user {
+      id
+      avatar
+      userName
+    }
+    files {
+      id
+      url
+    }
+    likeCount
+    isLiked
+    comments {
+      id
+      text
+      user {
+        id
+        userName
+      }
+    }
+    createdAt
+  }
+}
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  console.log(data, loading);
+  return 'feed';
+}
+
